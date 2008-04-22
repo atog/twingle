ddb29830c566f3af26f71c669f31b22e54ad797a
Koen Van der Auwera <atog@koen-van-der-auweras-macbook-pro-15.local>
2008-04-22 22:42:23 +0200

    inital version

---
+++ twingle.rb
@@ -0,0 +1,59 @@
+# atog@jabber.belnet.be
+
+Shoes.setup do
+  gem "xmpp4r-simple"
+end
+
+require "xmpp4r-simple"
+
+Shoes.app :width => 300, :height => 600, :resizable => true, :title => "Twingle, experience twitter" do
+  
+  def linkinizer(message)
+    if /http:\/\/\S+|@\S+/i =~ message
+      result = ""
+      pindex = 0
+      message.scan(/http:\/\/\S+|@\S+/i) do |l|
+        index = message.rindex(l)
+        result << "\"#{message[pindex, index-pindex]}\"," if index > 0
+        result << " link(\"#{l}\", :click => "
+        if /@(\S+)/ =~ l
+          result << "\"http://twitter.com/#{l[/@(\S+)/,1]}\"),"
+        else
+          result << "\"#{l}\"),"
+        end
+        pindex = index + l.length
+      end
+      result << " \"#{$'}\""
+      eval("para " + result)
+    else
+      para message
+    end    
+  end
+  
+  background "#eed"
+  
+  @jabber = Jabber::Simple.new("atog@jabber.belnet.be", "00xmpp42")  
+    
+  @main = stack do
+    background "#fff"
+    title "Twingle"
+  end
+  
+  stack :margin => 5 do
+    @say_it = edit_box :margin => 5, :width => 250, :height => 50, :size => 9
+    button "Say it" do
+      @jabber.deliver("twitter@twitter.com", @say_it.text)
+      @tweets.prepend { linkinizer("You! " + @say_it.text) }
+      @say_it.text = ''
+    end
+  end 
+    
+  @tweets = stack
+  
+  animate(1) do
+    @jabber.received_messages do |m|
+      @tweets.prepend { linkinizer(m.body) if m.from == "twitter@twitter.com" && m.type == :chat }
+    end    
+  end
+  
+end
