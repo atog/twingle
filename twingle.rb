b74cf41609290bbc27e322487d53e7bbd2e7b028
unknown <tom@.(none)>
2008-04-23 22:20:35 +0200

    Lots of UI changes.

--- twingle.rb
+++ twingle.rb
@@ -1,4 +1,5 @@
-# atog@jabber.belnet.be
+YOUR_JID = ""
+YOUR_PASSWORD = ""
 
 Shoes.setup do
   gem "xmpp4r-simple"
@@ -6,54 +7,119 @@ end
 
 require "xmpp4r-simple"
 
-Shoes.app :width => 300, :height => 600, :resizable => true, :title => "Twingle, experience twitter" do
+Shoes.app :width => 400, :height => 600, :resizable => true, :title => "Twingle, experience twitter" do
+
+  def twit(what)  
+    @tweets.prepend {     
+      flow :margin => 5 do 
+        background "#191616" .. "#363636", :radius => 8
+        stack :width => 58, :margin => 5 do
+          background "default_profile_normal.png", :width => 48, :height => 48, :radius => 4
+          image "spacer.gif", :width => 48, :height => 48
+        end
+        stack :width => -58, :margin => 5 do
+          eval("para " + linkinizer(what) + ", :stroke => '#fff', :margin => 0, :font => 'Arial 12px'") 
+        end 
+      end
+    }
+  end
+
+  def html(message)
+    message = message.gsub(/&(\w+);/) { |m|
+       case $1
+         when 'apos': "'"
+         when 'lt': "<"
+         when 'gt': ">"
+         when 'nbsp': " "
+         when 'quot': "\\\""
+         else m
+       end
+    }
+    message.gsub(/&amp;/, "&") 
+  end
   
   def linkinizer(message)
+    index = message.index(":")
+    result = "strong(\"#{message[0, index]}\"), "
+    message = message[index+1, message.length-index]
+
     if /http:\/\/\S+|@\S+/i =~ message
-      result = ""
       pindex = 0
       message.scan(/http:\/\/\S+|@\S+/i) do |l|
         index = message.rindex(l)
         result << "\"#{message[pindex, index-pindex]}\"," if index > 0
         result << " link(\"#{l}\", :click => "
         if /@(\S+)/ =~ l
-          result << "\"http://twitter.com/#{l[/@(\S+)/,1]}\"),"
+          result << "\'http://twitter.com/#{l[/@(\S+)/,1]}\'),"
         else
-          result << "\"#{l}\"),"
+          result << "'#{l}'),"
         end
         pindex = index + l.length
       end
       result << " \"#{$'}\""
-      eval("para " + result)
     else
-      para message
+      result << " \"" + message + "\""
     end    
+
+    result = result
+    result.gsub!(/"[^"]+"/) { |m| html(m) }
+    html(result)
+  end
+
+  def send_say_it 
+    @jabber.deliver("twitter@twitter.com", @say_it.text)
+    twit("You: " + @say_it.text)
+    @say_it.text = ''
   end
   
-  background "#eed"
-  
-  @jabber = Jabber::Simple.new("atog@jabber.belnet.be", "00xmpp42")  
-    
-  @main = stack do
-    background "#fff"
-    title "Twingle"
-  end
-  
-  stack :margin => 5 do
-    @say_it = edit_box :margin => 5, :width => 250, :height => 50, :size => 9
-    button "Say it" do
-      @jabber.deliver("twitter@twitter.com", @say_it.text)
-      @tweets.prepend { linkinizer("You! " + @say_it.text) }
-      @say_it.text = ''
+  background "#000"
+  flow :width => -15 do 
+    @header = stack do
+      background "#fff"
+      flow do
+        title "Twingle", :width => -110
+        stack :width => 100, :right => 0, :top => 5 do
+          @status = para strong("> <"), :stroke => '#F00'
+        end
+      end
     end
-  end 
-    
-  @tweets = stack
+  
+    flow do
+      background "#666" .. "#000"
+      flow :margin => 5 do
+        @say_it = edit_box :margin => 5, :width => -110, :height => 50, :size => 9 do
+          if @say_it.text[-1] == ?\n
+            send_say_it
+          end          
+        end
+        button "Say it", :width => 100, :right => 5, :top => 5 do
+          send_say_it
+        end
+      end 
+    end 
+      
+    @tweets = stack
+  end
+
+  @jabber = Jabber::Simple.new(YOUR_JID, YOUR_PASSWORD) 
+  @count = 0
   
   animate(1) do
-    @jabber.received_messages do |m|
-      @tweets.prepend { linkinizer(m.body) if m.from == "twitter@twitter.com" && m.type == :chat }
+    if @jabber.connected? 
+      @first = true
+      @jabber.received_messages do |m|
+        @chat_sound.play if @first 
+        @first = false
+        if m.from == "twitter@twitter.com" && m.type == :chat
+          @count += 1
+          twit(m.body)
+        end 
+      end
+      @status.replace strong(">-<(" + @count.to_s + ")"), :stroke => '#0C0'
+    else
+      @status.replace strong("> <"), :stroke => '#F00'
     end    
   end
   
+  @chat_sound = video 'chat2.flv', :width => 0, :height => 0
 end
