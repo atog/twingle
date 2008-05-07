b378f01e11ac76ab9d24317828e2853d484ae91f
Tom Adriaenssen <spam@inferis.org>
2008-05-07 12:08:32 +0200

    Added support for Rate Limit exceeded. Might need some more testing.

--- twingle.rb
+++ twingle.rb
@@ -19,32 +19,67 @@ class Twitson
   end
   
   def friends_timeline
-    get(@@friends_path)
+    get(@@friends_path, [])
   end
   
   def show(user)
-    get("#{@@show_path}#{user}.json")
+    get("#{@@show_path}#{user}.json", {})
   end
 
   protected
   
-    def get(path)
-      rvalue = []
-      begin
-        response = Net::HTTP.start(@@twitter, 80) do |http|
-          req = Net::HTTP::Get.new(path)
-          req.basic_auth(@username, @password) if !@username.empty?
-          http.request(req)
+    def get(path, default)
+      rvalue = default
+      
+      unless wait_for_rate_limit?
+        begin
+          response = Net::HTTP.start(@@twitter, 80) do |http|
+            req = Net::HTTP::Get.new(path)
+            req.basic_auth(@username, @password) if !@username.empty?
+            http.request(req)
+          end
+          if response.message == 'Bad Request' 
+            evalue = JSON.load(response.body)
+            raise StandardError if evalue['error'].index('Rate limit') === nil
+            # rate limit exceeded
+            extend_rate_limit
+          else
+            raise StandardError unless response.message == 'OK'
+          end
+          rvalue = JSON.load(response.body)
+        rescue StandardError => bang
+          error(response.body)
+          puts bang
         end
-        raise StandardError unless response.message == 'OK'
-        rvalue = JSON.load(response.body)
-      rescue StandardError => bang
-        puts bang
       end
       rvalue
     end  
+    
+    def extend_rate_limit
+      delay = { "until" => Time.now.to_i + 300 }
+      delay = { "delay" => delay } # wait 5 min
+      File.open( 'rate_limit_delay.yaml', 'w' ) do |out|
+        YAML.dump(delay, out)
+      end
+    end
+
+    def get_rate_limit_delay
+      delay = { }
+      if File.exists?('rate_limit_delay.yaml')
+        delay = YAML.load_file('rate_limit_delay.yaml')
+      end
+      
+      delay.key?("delay") ? delay["delay"]["value"] : 0
+    end
+
+    def wait_for_rate_limit?
+      wait = get_rate_limit_delay() > Time.now.to_i
+      File.delete 'rate_limit_delay.yaml' unless wait || !File.exists?('rate_limit_delay.yaml')
+      wait
+    end
 end
 
+Shoes.show_log
 Shoes.app :width => 400, :height => 600, :resizable => true, :title => "Twingle, experience twitter" do  
   
   @settings = YAML.load_file('twingle.yaml')
@@ -67,7 +102,7 @@ Shoes.app :width => 400, :height => 600, :resizable => true, :title => "Twingle,
         @avatars[username] = tweet["user"]["profile_image_url"]
         twit("#{username}: #{tweet['text']}", username)
       end
-      @avatars['You'] = @avatars[@settings["twitter"]["username"]] if @settings["twitter"]
+      #@avatars['You'] = @avatars[@settings["twitter"]["username"]] if @settings["twitter"]
       save_avatars  
     end
   end
@@ -79,12 +114,19 @@ Shoes.app :width => 400, :height => 600, :resizable => true, :title => "Twingle,
   end
   
   def avatar(user)
-    value = @avatars[user]
-    unless value
-      result = @twitson.show(user)
-      value = result["profile_image_url"]
-      @avatars[user] = value
-      save_avatars
+    value = "default_profile_normal.png"
+
+    if user 
+      user = @settings["twitter"]["username"] if user == 'You' && @settings["twitter"]
+      value = @avatars[user]
+      unless value
+        result = @twitson.show(user)
+        if result.key?("profile_image_url")
+          value = result["profile_image_url"]
+          @avatars[user] = value
+          save_avatars
+        end
+      end
     end
     value
   end
@@ -98,7 +140,7 @@ Shoes.app :width => 400, :height => 600, :resizable => true, :title => "Twingle,
       flow :margin => 5 do 
         background "#191616" .. "#363636", :radius => 8
         stack :width => 58, :margin => 5 do
-          avatar = (user && avatar(user)) ? avatar(user) : "default_profile_normal.png"
+          avatar = avatar(user)
           image avatar, :width => 48, :height => 48, :radius => 4
         end
         stack :width => -58, :margin => 5 do
